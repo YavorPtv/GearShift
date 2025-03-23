1d658730a370a205b965c490a4470b43790193e3
YavorPtv <yavorpetrakiev@gmail.com>
2025-03-23 17:21:26 +0200

    Add login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,26 @@
+import { useNavigate } from "react-router";
+import { useLogin } from "../../api/authApi";
+import { useUserContext } from "../../contexts/UserContext";
+
 export default function Login() {
+    const navigate = useNavigate();
+    const { login } = useLogin();
+    const { userLoginHandler } = useUserContext();
+
+    const loginHandler = async (formData) => {
+        const {email, password} = Object.fromEntries(formData);
+
+        const authData = await login(email, password);
+
+        userLoginHandler(authData);
+
+        navigate('/');
+    }
+
     return (
         <div className="login-form-container">
             <h2>Login</h2>
-            <form>
+            <form action={loginHandler}>
                 <div className="login-form-group">
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" placeholder="Enter your email" required />
