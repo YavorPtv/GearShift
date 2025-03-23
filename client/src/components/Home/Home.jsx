375dab9047ddad197637169020a26ebb27ef9467
YavorPtv <yavorpetrakiev@gmail.com>
2025-03-23 23:05:54 +0200

    Fix home page call to action buttons and styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function Home() {
     return (
         <section id="home" className="welcome-hero">
@@ -5,12 +7,8 @@ export default function Home() {
                 <div className="welcome-hero-txt">
                     <h2>Shift Into High Gear</h2>
                     <p>Discover, showcase, and discuss the world's best rides.</p>
-                    <button className="welcome-btn" href="/catalog">
-                        Explore cars
-                    </button>
-                    <button className="welcome-btn" href="/register">
-                        Join the Community
-                    </button>
+                    <Link to="/cars" className="welcome-btn">Explore cars</Link>
+                    <Link to="/register" className="welcome-btn">Join the Community</Link>
                 </div>
             </div>
         </section>
