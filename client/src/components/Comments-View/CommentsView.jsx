586149dd3ffb6b2327a4e9dcd5e8e94f30888202
YavorPtv <yavorpetrakiev@gmail.com>
2025-03-29 02:03:50 +0200

    Add edit and delete buttons for owned comments

--- client/src/components/Comments-View/CommentsView.jsx
+++ client/src/components/Comments-View/CommentsView.jsx
@@ -1,21 +1,38 @@
+import { FaEdit, FaTrash } from "react-icons/fa"; // Import icons
+import useAuth from "../../hooks/useAuth";
+
 export default function CommentsView({
-    comments
+    comments,
 }) {
+    const { userId } = useAuth();
+    
     return (
         <>
             <h2 className="section-title">COMMENTS</h2>
 
             <div className="comments-container">
                 {comments.length > 0
-                    ? comments.map(({_id, comment, author}) => (
+                    ? comments.map(({ _id, comment, author, _ownerId }) => (
                         <div key={_id} className="comment">
-                            <div className="username">{author.username}</div>
+                            <div className="comment-header">
+                                <div className="username">{author.username}</div>
+                                { _ownerId === userId && (
+                                    <div className="comment-actions">
+                                        <button className="comment-edit-button">
+                                            <FaEdit />
+                                        </button>
+                                        <button className="comment-delete-button">
+                                            <FaTrash />
+                                        </button>
+                                    </div>  
+                                )}
+
+                            </div>
                             <div className="comment-text">{comment}</div>
                         </div>
                     ))
                     : <h4>No comments!</h4>
                 }
-                
             </div>
         </>
     );
